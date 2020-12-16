af5155a536aee13591e3ac1499006b59658c4992
syedsali <syedsali0315@gmail.com>
2020-12-15 17:06:29 -0700

    index.html

---
+++ app.js
@@ -0,0 +1,60 @@
+// let number = [9, 10, 11, 12];
+// console.log(number);
+
+// let strings= new Array("lime", "lemon", "oranges", "grapefruit");
+// console.log(strings);
+
+// let booleans= new Array()
+// booleans[0]= 9<10;
+// booleans[1]= 11===11.5;
+// booleans[2]= 12>8;
+// booleans[3]= "syed"!="omar";
+// console.log(booleans);
+
+let TheDonutShop= new Array() 
+TheDonutShop[0]= "santa belly";  
+TheDonutShop[1]= "festive tree";
+TheDonutShop[2]= "Present";
+TheDonutShop[3]= "oreo cookies";
+TheDonutShop[4]= "birthday batter";
+TheDonutShop[5]= "reeses classic";
+
+
+
+
+// document.getElementById("dshop").innerHTML =DonutShop.join("Donut");
+console.log(TheDonutShop.indexOf("reeses classic"));
+
+for (i=0; i<TheDonutShop.length; i++){
+    console.log(TheDonutShop[i]);
+}
+// let DonutShop = [" Glaze ", " Chocolate glazed", " Penaut butter", "Strawberry sprinker"];
+// let toptwo = DonutShop.slice(1); 
+// // let toptwo = DonutShop.slice(1);
+// document.getElementById("lice").innerHTML= DonutShop
+
+// DonutShop.shift();
+// DonutShop.pop();
+// DonutShop.unshift("lime zest");
+// DonutShop.push("lemon pie");
+// var DonutShop = [" glaze ", " chocolate glaze ", " penut butter ", " strawberry sprinkle ", " oreo filled ", " lemon pie ", " boston Cream "]
+// document.getElementById("dshop").innerHTML = DonutShop.join("Donuts");
+
+
+
+
+
+// console.log(classRoom.indexOf("Ngoc")); this will locate your data 
+// classRoom.splice(3, 1, "Minah"); this will allow you to replace your data or remove with a new data from anywhere in your data
+// classRoom.unshift("Minah"); this will add an item on the top of you list
+// classRoom.shift(); this will remove the top listed item
+// classRoom.push("Omar"); this will add an additional item in your list
+// classRoom.pop(); this will delete the last item from your list
+
+// this is how you create loop
+// there are 3 types of loops 
+// 1) for loop
+// 2) while loop
+// 3) foreach loop
+
+
